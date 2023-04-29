0aaf915840b48e4707fd9cd11f604d18857461a7
Mark Wilkins <mwilkins@wealthsimple.ca>
2023-04-29 12:33:12 -0400

    Makes headers object extend the actual headers object

--- src/http-headers.class.ts
+++ src/http-headers.class.ts
@@ -1,11 +1,11 @@
-import { isNil, omitBy } from 'lodash';
+import { capitalize, filter, isNil } from 'lodash';
 import { Age } from './age.class';
 import { CacheControlHeader } from './cache-control.class';
 import { ClearSiteData } from './clear-site-data.class';
 import { Expires } from './expires.class';
 import { HTTP_STATUS_CODE } from './http-status-codes';
 
-export class HttpHeaders {
+export class HttpHeaders extends Headers {
   protected cacheControl?: CacheControlHeader;
   protected age?: Age;
   protected clearSiteData?: ClearSiteData;
@@ -20,26 +20,37 @@ export class HttpHeaders {
       Expires: (builder: Expires) => Expires;
     }>
   ) {
-    this.cacheControl = headers?.['Cache-Control']?.(
-      new CacheControlHeader({})
-    );
+    const headersInit = filter(
+      [
+        [
+          'Cache-Control',
+          headers?.['Cache-Control']?.(new CacheControlHeader({})).build(),
+        ],
+        ['Age', headers?.['Age']?.(new Age(0)).build()],
+        [
+          'Clear-Site-Data',
+          headers?.['Clear-Site-Data']?.(new ClearSiteData({})).build(),
+        ],
+        ['Expires', headers?.['Expires']?.(new Expires(new Date())).build()],
+      ],
+      ([, value]) => !isNil(value)
+    ) as [string, string][];
 
-    this.age = headers?.['Age']?.(new Age(0));
-
-    this.clearSiteData = headers?.['Clear-Site-Data']?.(new ClearSiteData({}));
-
-    this.expires = headers?.['Expires']?.(new Expires(new Date()));
+    super(headersInit);
   }
 
   getHeadersObject() {
-    return omitBy(
-      {
-        'Cache-Control': this.cacheControl?.build(),
-        Age: this.age?.build(),
-        'Clear-Site-Data': this.clearSiteData?.build(),
-        Expires: this.expires?.build(),
-      },
-      isNil
-    ) as Record<string, string>;
+    const headers: Record<string, string> = {};
+    this.forEach((value, key) => {
+      headers[capitalizeWords(key)] = value;
+    });
+
+    return headers;
   }
+}
+
+function capitalizeWords(input: string) {
+  const words = input.split(/[-_]/); // Split the string by hyphen or underscore
+  const capitalizedWords = words.map(word => capitalize(word)); // Capitalize each word using lodash's capitalize method
+  return capitalizedWords.join('-'); // Join the words with hyphen to form the final string
 }
