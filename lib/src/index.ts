7ecaacdf9ddee8240e5a0a5c87428934f6f2e7b0
luckyboy125 <117144469+luckyboy125@users.noreply.github.com>
2023-05-03 18:19:55 +0300

    fixed conflict in index .ts

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -12,7 +12,6 @@ export * from "./keep-alive.class";
 export * from "./content-type.class";
 export * from "./expires.class";
 export * from "./http-headers.class";
-export * from "./proxy-authenticate.class";
 export * from "./trailer.class";
 export * from "./transfer-encoding.class";
 export * from "./te.class";
