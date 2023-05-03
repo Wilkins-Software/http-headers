26cf3729cf658bbcd48d2eb9ff044f54c3444bab
luckyboy125 <117144469+luckyboy125@users.noreply.github.com>
2023-05-03 05:19:43 +0300

    connection-implement

---
+++ lib/test/connection.spec.ts
@@ -0,0 +1,20 @@
+import { Connection } from "../src/connection.class";
+
+describe("Connection", () => {
+  it("should build a valid Connection header", () => {
+    const connection = new Connection("keep-alive");
+    expect(connection.build()).toBe("keep-alive");
+  });
+
+  it("should set a valid Connection header", () => {
+    const connection = new Connection("keep-alive");
+    expect(connection.setConnection("close").build()).toBe("close");
+  });
+
+  it("should build a valid Connection header type", () => {
+    const connection = new Connection("keep-alive");
+    expect(connection.getHeadersObject()).toEqual({
+      Connection: "keep-alive",
+    });
+  });
+});
