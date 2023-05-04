b7635e438e20d39d2fb4b57453aed960d770a4f3
luckyboy125 <117144469+luckyboy125@users.noreply.github.com>
2023-05-04 03:59:27 +0300

    Fixed wrong name of trailer file name, changed test case

--- lib/test/trailer.spec.ts
+++ lib/test/trailer.spec.ts
@@ -4,18 +4,14 @@ describe("Trailer", () => {
   it("should build a valid Trailer header", () => {
     const trailer = new Trailer("Expires");
     expect(trailer.build()).toBe("Expires");
-  });
-
-  it("should build a valid Trailer header object", () => {
-    const trailer = new Trailer("Expires");
     expect(trailer.getHeadersObject()).toEqual({
       Trailer: "Expires",
     });
   });
 
   it("Allows you to construct the trailer with a builder function", () => {
-    const hoursTrailer = new Trailer("");
-    hoursTrailer.setTrailer("Expries");
-    expect(hoursTrailer.build()).toBe("Expries");
+    const trailer = new Trailer("");
+    trailer.setTrailer("Expries");
+    expect(trailer.build()).toBe("Expries");
   });
 });
