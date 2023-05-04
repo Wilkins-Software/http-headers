d017f6111ea0dfc23dc5c16fdfae12a0a0bbf45d
luckyboy125 <117144469+luckyboy125@users.noreply.github.com>
2023-05-04 15:13:25 +0300

    update accept-ranges-spec

--- lib/test/accept-ranges.spec.ts
+++ lib/test/accept-ranges.spec.ts
@@ -2,15 +2,15 @@ import { AcceptRanges } from "../src/accept-ranges.class";
 
 describe("AcceptRanges", () => {
   it("should build a valid AcceptRanges header", () => {
-    const connection = new AcceptRanges("bytes");
-    expect(connection.build()).toBe("bytes");
-    expect(connection.getHeadersObject()).toEqual({
+    const acceptRanges = new AcceptRanges("bytes");
+    expect(acceptRanges.build()).toBe("bytes");
+    expect(acceptRanges.getHeadersObject()).toEqual({
       "Accept-Ranges": "bytes",
     });
   });
 
   it("should set a valid AcceptRanges header", () => {
-    const connection = new AcceptRanges("bytes");
-    expect(connection.setAcceptRanges("none").build()).toBe("none");
+    const acceptRanges = new AcceptRanges("bytes");
+    expect(acceptRanges.setAcceptRanges("none").build()).toBe("none");
   });
 });
