d568f283899ca7126e410940cce53bb1c1e2ba63
luckyboy125 <117144469+luckyboy125@users.noreply.github.com>
2023-05-04 11:20:21 +0300

    modify-test-accept-encoding

--- lib/test/accept-encoding.spec.ts
+++ lib/test/accept-encoding.spec.ts
@@ -2,45 +2,47 @@ import { AcceptEncoding } from "../src/accept-encoding.class";
 
 describe("AcceptEncoding", () => {
   it("builds a AcceptEncoding header", () => {
-    const te = new AcceptEncoding({
+    const acceptEncoding = new AcceptEncoding({
       gzip: true,
       compress: true,
       deflate: 0.5,
       br: true,
       identity: 1.0,
     });
-    expect(te.build()).toBe("gzip, compress, deflate;q=0.5, br, identity;q=1");
-    expect(te.getHeadersObject()).toEqual({
+    expect(acceptEncoding.build()).toBe(
+      "gzip, compress, deflate;q=0.5, br, identity;q=1"
+    );
+    expect(acceptEncoding.getHeadersObject()).toEqual({
       "Accept-Encoding": "gzip, compress, deflate;q=0.5, br, identity;q=1",
     });
   });
 
   it("builds valid setter AcceptEncoding header", () => {
-    const te = new AcceptEncoding({
+    const acceptEncoding = new AcceptEncoding({
       gzip: true,
       compress: true,
       deflate: 0.5,
       br: true,
       identity: 1.0,
     });
-    te.setProperty("identity", 0.8);
-    expect(te.build()).toBe(
+    acceptEncoding.setProperty("identity", 0.8);
+    expect(acceptEncoding.build()).toBe(
       "gzip, compress, deflate;q=0.5, br, identity;q=0.8"
     );
 
-    te.setAllProperty({
+    acceptEncoding.setAllProperty({
       deflate: 0.1,
       identity: 0.2,
     });
-    expect(te.getHeadersObject()).toEqual({
+    expect(acceptEncoding.getHeadersObject()).toEqual({
       "Accept-Encoding": "deflate;q=0.1, identity;q=0.2",
     });
   });
 
   it("builds a AcceptEncoding header with *", () => {
-    const te = new AcceptEncoding("*");
-    expect(te.build()).toBe("*");
-    expect(te.getHeadersObject()).toEqual({
+    const acceptEncoding = new AcceptEncoding("*");
+    expect(acceptEncoding.build()).toBe("*");
+    expect(acceptEncoding.getHeadersObject()).toEqual({
       "Accept-Encoding": "*",
     });
   });
