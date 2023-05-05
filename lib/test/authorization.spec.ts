fa623033aaafba68e798062440a318afaded678a
luckyboy125 <117144469+luckyboy125@users.noreply.github.com>
2023-05-05 07:19:21 +0300

    modify mock key as mock form in Digest method

--- lib/test/authorization.spec.ts
+++ lib/test/authorization.spec.ts
@@ -2,18 +2,12 @@ import { Authorization } from "../src/authorization.class";
 
 describe("Authorization", () => {
   it("builds a Authorization header in basic case", () => {
-    const authorization = new Authorization(
-      "Basic",
-      "YWxhZGRpbjpvcGVuc2VzYW1l"
-    );
-    expect(authorization.build()).toBe("Basic YWxhZGRpbjpvcGVuc2VzYW1l");
+    const authorization = new Authorization("Basic", "<credentials-key>");
+    expect(authorization.build()).toBe("Basic <credentials-key>");
 
-    authorization.setAuthorization(
-      "Basic",
-      "ODFjOTBkZjJhOTliN2RiNzk5MzlmZTc3NmUyYjFmMDI"
-    );
+    authorization.setAuthorization("Basic", "<credentials-other-key>");
     expect(authorization.getHeadersObject()).toEqual({
-      Authorization: "Basic ODFjOTBkZjJhOTliN2RiNzk5MzlmZTc3NmUyYjFmMDI",
+      Authorization: "Basic <credentials-other-key>",
     });
   });
 
@@ -21,33 +15,33 @@ describe("Authorization", () => {
     const authorization = new Authorization("Digest", {
       username: "User123",
       realm: "example.com",
-      nonce: "MTYyOTI4Njg1NjY3MzozODg0MzYxN2RjOTExMzJjMDYzOGJkZTE3NjI4NDAxMw==",
+      nonce: "<credentials-nonce-key>",
       uri: "/example",
       qop: "auth",
       nc: 1,
-      cnonce: "ODFjOTBkZjJhOTliN2RiNzk5MzlmZTc3NmUyYjFmMDI=",
-      response: "51dd120c5c7fd99fa4a153f5c8bc0c30",
-      opaque: "05a3d0299c7977d1e821c2a4ed14eaa5",
+      cnonce: "<credentials-nc-key>",
+      response: "<credentials-response-key>",
+      opaque: "<credentials-opaque-key>",
       algorithm: "MD5",
     });
     expect(authorization.build()).toBe(
-      `Digest username="User123", realm="example.com", nonce="MTYyOTI4Njg1NjY3MzozODg0MzYxN2RjOTExMzJjMDYzOGJkZTE3NjI4NDAxMw==", uri="/example", qop="auth", nc="1", cnonce="ODFjOTBkZjJhOTliN2RiNzk5MzlmZTc3NmUyYjFmMDI=", response="51dd120c5c7fd99fa4a153f5c8bc0c30", opaque="05a3d0299c7977d1e821c2a4ed14eaa5", algorithm="MD5"`
+      `Digest username="User123", realm="example.com", nonce="<credentials-nonce-key>", uri="/example", qop="auth", nc="1", cnonce="<credentials-nc-key>", response="<credentials-response-key>", opaque="<credentials-opaque-key>", algorithm="MD5"`
     );
 
     authorization.setAuthorization("Digest", {
       username: "DigestSetTest",
       realm: "digest_test.com",
-      nonce: "aEdreDWERWDSFY3MzozODg0MzYxN2RjOTExMzJjMDYzOGJkZTE3NjI4NDAxMw==",
+      nonce: "<credentials-nonce-key-other>==",
       uri: "/digest_test",
       qop: "auth-int",
       nc: 9,
-      cnonce: "EHJDjOTBkZjJhOTliN2RiNzk5MzlmZTc3NmUyYjFmMDI=",
-      response: "984d120c5c7fd99fa4a153f5c8bc0c30",
-      opaque: "DERD3d0299c7977d1e821c2a4ed14eaa5",
+      cnonce: "<credentials-nc-key-other>",
+      response: "<credentials-response-key-other>",
+      opaque: "<credentials-opaque-key-other>",
       algorithm: "MD6",
     });
     expect(authorization.getHeadersObject()).toEqual({
-      Authorization: `Digest username="DigestSetTest", realm="digest_test.com", nonce="aEdreDWERWDSFY3MzozODg0MzYxN2RjOTExMzJjMDYzOGJkZTE3NjI4NDAxMw==", uri="/digest_test", qop="auth-int", nc="9", cnonce="EHJDjOTBkZjJhOTliN2RiNzk5MzlmZTc3NmUyYjFmMDI=", response="984d120c5c7fd99fa4a153f5c8bc0c30", opaque="DERD3d0299c7977d1e821c2a4ed14eaa5", algorithm="MD6"`,
+      Authorization: `Digest username="DigestSetTest", realm="digest_test.com", nonce="<credentials-nonce-key-other>==", uri="/digest_test", qop="auth-int", nc="9", cnonce="<credentials-nc-key-other>", response="<credentials-response-key-other>", opaque="<credentials-opaque-key-other>", algorithm="MD6"`,
     });
   });
 });
