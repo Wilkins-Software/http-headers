0166d024434ecfa67012d17a3b427aac770d9943
luckyboy125 <117144469+luckyboy125@users.noreply.github.com>
2023-05-05 11:50:21 +0300

    modify test case

--- lib/test/content-length.spec.ts
+++ lib/test/content-length.spec.ts
@@ -4,9 +4,9 @@ describe("ContentLength", () => {
   it("builds a ContentLength header", () => {
     const contentEncoding = new ContentLength(123);
     expect(contentEncoding.build()).toBe("123");
-    contentEncoding.setLength(1234);
+    contentEncoding.setLength(789);
     expect(contentEncoding.getHeadersObject()).toEqual({
-      "Content-Length": "1234",
+      "Content-Length": "789",
     });
   });
 });
