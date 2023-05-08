b53aa3fa121d34713d4cd4e0e8ae69c87b8142f9
luckyboy125 <117144469+luckyboy125@users.noreply.github.com>
2023-05-09 02:28:02 +0300

    sec-ch-ua-bitness-implement

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -43,4 +43,5 @@ export * from "./content-range.class";
 export * from "./content-security-policy.class";
 export * from "./cookie.class";
 export * from "./early-data.class";
+export * from "./sec-ch-ua-bitness.class";
 export * from "./http-status-codes";
