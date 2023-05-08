a27d2184f78ccf2c75c10a6caeefaf3188ab4628
luckyboy125 <117144469+luckyboy125@users.noreply.github.com>
2023-05-08 04:44:58 +0300

    if-modified-since-implement

---
+++ lib/test/if-modified-since.spec.ts
@@ -0,0 +1,12 @@
+import { IfModifiedSince } from "../src/if-modified-since.class";
+
+describe("IfModifiedSince", () => {
+  it("should build a valid IfModifiedSince header", () => {
+    const date = new IfModifiedSince("Wed, 20 May 2023 12:00:00 GMT");
+    expect(date.build()).toBe("Wed, 20 May 2023 12:00:00 GMT");
+    date.setDate("Wed, 21 Oct 2015 07:28:00 GMT");
+    expect(date.getHeadersObject()).toEqual({
+      "If-Modified-Since": "Wed, 21 Oct 2015 07:28:00 GMT",
+    });
+  });
+});
