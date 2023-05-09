407e47e6d04f60db122baf9048c8a656f8253496
luckyboy125 <117144469+luckyboy125@users.noreply.github.com>
2023-05-09 05:56:11 +0300

    via-implement

---
+++ lib/src/via.class.ts
@@ -0,0 +1,48 @@
+import { BaseHeader } from "./base-header.class";
+
+export type ViaType = {
+  name?: string;
+  version?: string;
+  host?: string;
+  port?: string;
+  pseudonym?: string;
+};
+
+export class Via extends BaseHeader {
+  protected _via?: string;
+
+  constructor(...viaRes: ViaType[]) {
+    super();
+    this._via = viaRes
+      .map(
+        (via) =>
+          `${via.name ? via.name + "/" : ""}${via.version ? via.version : ""}${
+            via.pseudonym ? " " + via.pseudonym : ""
+          }${via.host ? " " + via.host : ""}${via.port ? ":" + via.port : ""}`
+      )
+      .join(", ");
+  }
+
+  getHeadersObject(): Record<string, string> {
+    return {
+      Via: this.build(),
+    };
+  }
+
+  build(): string {
+    if (this._via) return this._via;
+    return "null";
+  }
+
+  setVia(...newViaRes: ViaType[]) {
+    this._via = newViaRes
+      .map(
+        (via) =>
+          `${via.name ? via.name + "/" : ""}${via.version ? via.version : ""}${
+            via.pseudonym ? " " + via.pseudonym : ""
+          }${via.host ? " " + via.host : ""}${via.port ? ":" + via.port : ""}`
+      )
+      .join("; ");
+    return this;
+  }
+}
